4b672a96c75f4096c0bdb5713aff89e7039b8001
angreno <singhkunaliitianbusiness1@gmail.com>
2024-01-18 16:08:11 +0530

    update files

---
+++ c7_innerProduct_arrat.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+a = np.array([6,2,3])
+b = np.array([2,5,2])
+print("Original 1 d arrray:",a,"\ninner product of array:",np.inner(a,b))
+
---
+++ c8_innerProduct_matrices.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+a = np.array(([3,6,4],[9,4,6]))
+b=np.array(([1,15,6],[3,10,8]))
+
+
+print("inner product :\n",np.inner(a,b))
+
+print("outer product: \n",np.outer(a,b))
+
+
